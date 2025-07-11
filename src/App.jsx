e56088fb33af039be65e2d151ad52336f40016cb
namrata <namrataap725@gmail.com>
2025-07-11 20:43:22 +0545

    Replace BrowserRouter with HashRouter to prevent 404 errors on page refresh in GitHub Pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import MainPage from "./component/MainPage";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Mealinfo from "./component/MealInfo";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import MainPage from "./MainPage";
+import Mealinfo from "./Mealinfo";
+
 function App() {
   return (
-    <>
-      {/* <MainPage /> */}
-      <Router>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/:idMeal" element={<Mealinfo />} />
-        </Routes>
-      </Router>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/:idMeal" element={<Mealinfo />} />
+      </Routes>
+    </Router>
   );
 }
 
